498b9b2ecb289a740074827b95c468407558971d
Sackitey68 <sackitey68@gmail.com>
2024-03-23 15:12:31 +0000

    fix error in the statusCode

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-  const statusCode = res.statusCode || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message ||'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
