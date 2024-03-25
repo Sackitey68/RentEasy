e7d776a955396dc1212fa51fc65d9e7f2cc3053e
Sackitey68 <sackitey68@gmail.com>
2024-03-25 11:35:49 +0000

    create user update api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from './routes/auth.route.js';
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -10,12 +11,15 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
   })
-  .catch((err) => {
+  .catch((err) => {  
     console.log(err);
   });
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
@@ -26,11 +30,10 @@ app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message ||'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
 });
-
